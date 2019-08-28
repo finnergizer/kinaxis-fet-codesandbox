5c5b1d9fc7e2155cb2f8f106f8579c15227b08c0
Shaughn Finnerty <Shaughn.finnerty@gmail.com>
2019-08-28 03:26:26 -0400

    Add pagination

--- src/App/index.js
+++ src/App/index.js
@@ -1,10 +1,18 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableHead from "@material-ui/core/TableHead";
+import TableFooter from "@material-ui/core/TableFooter";
 import TableRow from "@material-ui/core/TableRow";
+import TablePagination from "@material-ui/core/TablePagination";
 import TextField from "@material-ui/core/TextField";
+import IconButton from "@material-ui/core/IconButton";
+import FirstPageIcon from "@material-ui/icons/FirstPage";
+import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
+import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import LastPageIcon from "@material-ui/icons/LastPage";
 
 export class App extends React.Component {
   render() {
@@ -23,19 +31,26 @@ class NbaPlayersTable extends React.Component {
     super(props);
     this.state = {
       players: [],
-      searchValue: ""
+      searchValue: "",
+      rowsPerPage: 5,
+      currentPage: 0
     };
   }
 
   componentDidMount() {
-    this.queryPlayers(this.state.searchValue);
+    this.queryPlayers(
+      this.state.searchValue,
+      this.state.currentPage,
+      this.state.rowsPerPage
+    );
   }
 
   handleChange = event => {
     this.setState({
-      searchValue: event.target.value
+      searchValue: event.target.value,
+      currentPage: 0
     });
-    this.queryPlayers(event.target.value);
+    this.queryPlayers(event.target.value, 0, this.state.rowsPerPage);
   };
 
   render() {
@@ -67,20 +82,64 @@ class NbaPlayersTable extends React.Component {
               </TableRow>
             ))}
           </TableBody>
+
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25]}
+                colSpan={4}
+                count={this.state.count}
+                rowsPerPage={this.state.rowsPerPage}
+                page={this.state.currentPage}
+                SelectProps={{
+                  inputProps: { "aria-label": "rows per page" },
+                  native: true
+                }}
+                onChangePage={this.handleChangePage}
+                onChangeRowsPerPage={this.handleChangeRowsPerPage}
+                // ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
         </Table>
       </div>
     );
   }
 
-  queryPlayers(searchValue) {
-    fetch("https://www.balldontlie.io/api/v1/players?search=" + searchValue)
+  handleChangePage = (event, newPage) => {
+    this.setState({
+      currentPage: newPage
+    });
+    this.queryPlayers(this.state.searchValue, newPage, this.state.rowsPerPage);
+  };
+
+  handleChangeRowsPerPage = event => {
+    this.setState({
+      rowsPerPage: parseInt(event.target.value, 10),
+      currentPage: 0
+    });
+    this.queryPlayers(
+      this.state.searchValue,
+      0,
+      parseInt(event.target.value, 10)
+    );
+  };
+
+  queryPlayers(searchValue, page, rowsPerPage) {
+    fetch(
+      "https://www.balldontlie.io/api/v1/players?search=" +
+        searchValue +
+        (page ? "&page=" + page + 1 : "") +
+        (rowsPerPage ? "&per_page=" + rowsPerPage : "")
+    )
       .then(res => {
         return res.json();
       })
       .then(
         result => {
           this.setState({
-            players: result.data
+            players: result.data,
+            count: result.meta["total_count"]
           });
         },
         error => {
@@ -92,3 +151,63 @@ class NbaPlayersTable extends React.Component {
       );
   }
 }
+
+function TablePaginationActions(props) {
+  const { count, page, rowsPerPage, onChangePage } = props;
+
+  function handleFirstPageButtonClick(event) {
+    onChangePage(event, 0);
+  }
+
+  function handleBackButtonClick(event) {
+    onChangePage(event, page - 1);
+  }
+
+  function handleNextButtonClick(event) {
+    onChangePage(event, page + 1);
+  }
+
+  function handleLastPageButtonClick(event) {
+    onChangePage(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  }
+
+  return (
+    <div>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        <FirstPageIcon />
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        <KeyboardArrowLeft />
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        <KeyboardArrowRight />
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        <LastPageIcon />
+      </IconButton>
+    </div>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number,
+  onChangePage: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired
+};
