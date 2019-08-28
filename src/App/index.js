aa3f29a44b072415e78268307ccac37ad15f98a1
Shaughn Finnerty <Shaughn.finnerty@gmail.com>
2019-08-27 23:02:58 -0400

    Initial commit

--- src/App/index.js
+++ src/App/index.js
@@ -1,8 +1,10 @@
+import React from "react";
+
 export function App() {
   return (
     <div className="App">
       <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <h2>This will be the start of the demo.</h2>
     </div>
   );
 }
